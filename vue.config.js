73911902a4f66819ef03315543d10c3b773f5dbf
zpony <877473043@qq.com>
2021-01-21 13:03:30 +0800

    vue修改

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const Webpack = require("webpack");
+const WebpackBar = require("webpackbar");
 const resolve = dir => path.join(__dirname, dir);
 module.exports = {
   devServer: { port: 9090 },
@@ -10,7 +11,33 @@ module.exports = {
         alias: {
           "@": resolve("src")
         }
-      }
+      },
+      plugins: [new WebpackBar()]
     };
+  },
+  chainWebpack(config) {
+    config.when(process.env.NODE_ENV !== "development", config => {
+      config.optimization.splitChunks({
+        chunks: "all",
+        cacheGroups: {
+          libs: {
+            name: "chunk-libs",
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: "initial"
+          },
+          elementUI: {
+            name: "chunk-elementUI",
+            priority: 20,
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/
+          },
+          fortawesome: {
+            name: "chunk-fortawesome",
+            priority: 20,
+            test: /[\\/]node_modules[\\/]_?@fortawesome(.*)/
+          }
+        }
+      });
+    });
   }
 };
